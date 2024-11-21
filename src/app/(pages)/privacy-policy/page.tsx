51fec97c9ba16d7e74e16ed1c6739e71a0446637
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-11-21 16:08:10 +0530

    ui update

--- src/app/(pages)/privacy-policy/page.tsx
+++ src/app/(pages)/privacy-policy/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./style.css";
 
 export default function PrivacyPolicy() {
   return <main className='privacyPolicyContainer'>PrivacyPolicy</main>;
--- src/app/(pages)/terms-of-use/page.tsx
+++ src/app/(pages)/terms-of-use/page.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import "./style.css";
-import "../../style.css";
 
 export default function TermsOfUse() {
   return (
